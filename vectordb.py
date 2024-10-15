9d1c537c84bfec756ac8a8d42378663f43766a99
peteykienzzz <pmatczak2@gmail.com>
2024-10-14 17:38:37 -0700

    "Implement vectordb for chess rules querying
    - Create vectordb.py for handling chess rule queries
    - Set up ChromaDB with SentenceTransformer embedding
    - Implement functions for loading data and running queries
    - Add system_info.py for system-related information
    - Update .gitignore to exclude virtual environment files"

--- vectordb.py
+++ vectordb.py
@@ -1,17 +1,25 @@
-# pip install chromadb
 import pprint
 import re
 import chromadb
 from chromadb.config import Settings
+from chromadb.utils import embedding_functions
+
 
 RELOAD_DB = True
 
 chroma_client = chromadb.PersistentClient(path="chroma.db",
                                           settings=Settings(
                                               anonymized_telemetry=False
-                                          )
-                                          )
-collection = chroma_client.get_or_create_collection(name="chess")
+                                          ))
+
+# Define a new embedding function
+embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+# Use the new embedding function when creating or getting the collection
+collection = chroma_client.get_or_create_collection(
+    name="chess",
+    embedding_function=embedding_function
+)
+
 
 
 def query_source_data():
@@ -37,11 +45,12 @@ def run_query(query: str):
 
 
 def main():
+    
     if RELOAD_DB:
         results = query_source_data()
         load_data(results)
     questions = [
-        "how can a bishop capture another piece",
+        "What happens if a pawn reaches the other side of the board?",
     ]
     for question in questions:
         findings = run_query(question)
